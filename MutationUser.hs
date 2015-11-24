0c0b8392ef60a0c05a06e806452fdbe839efe09b
LiquidWater <LiquidWater@localhost>
2015-11-23 21:56:25 -0500

    Updated with david's changes to starter code

--- MutationUser.hs
+++ MutationUser.hs
@@ -4,7 +4,7 @@ This file contains code which uses the mutation library found in Mutation.hs
 -}
 
 import Mutation (
-    get, set, def, Mutable, Pointer
+    get, set, def, Mutable, Pointer, Memory
     )
 
 -- | Takes a number <n> and memory, and stores two new values in memory:
@@ -12,6 +12,6 @@ import Mutation (
 --   - the boolean (n > 0) at location 500
 --   Return the pointer to each stored value, and the new memory.
 --   You may assume these locations are not already used by the memory.
-pointerTest :: Integer -> Memory -> ((Pointer Integer, Pointer Bool), Memory)   
+pointerTest :: Integer -> Memory -> ((Pointer Integer, Pointer Bool), Memory)
 pointerTest = undefined
 
