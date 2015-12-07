de7a2e033b6ac9f86743c65f71da7493609c2640
LiquidWater <LiquidWater@192.168.2.4>
2015-12-07 14:09:16 -0500

    Possibly finished part1

--- Mutation.hs
+++ Mutation.hs
@@ -7,9 +7,9 @@ which you will use as the data structure for storing "mutable" data.
 -- **YOU MUST ADD ALL FUNCTIONS AND TYPES TO THIS LIST AS YOU CREATE THEM!!**
 module Mutation (
     Mutable, get, set, def,
-    Memory, Pointer,
+    Memory, Pointer(..),
     runOp, (>>>), (>~>),
-    Value, StateOp, returnVal
+    Value(..), StateOp
     )
     where
 
@@ -30,25 +30,26 @@ data Pointer a = P Integer
 -- Type class representing a type which can be stored in "Memory".
 class Mutable a where
     -- Look up a value in memory referred to by a pointer.
-    get :: AList Integer a -> Pointer a -> a
-    get mem (P addr) = lookupA mem addr
+    get :: Memory -> Pointer a -> a
 
     -- Change a value in memory referred to by a pointer.
     -- Return the new memory after the update.
-    set :: AList Integer a -> Pointer a -> a -> AList Integer a
-    set mem (P addr) val = updateA mem (addr, val)
+    set :: Memory -> Pointer a -> a -> Memory
 
     -- Create a new memory location storing a value, returning a new pointer
     -- and the new memory with the new value.
     -- Raise an error if the input Integer is already storing a value.
-    def :: AList Integer a -> Integer -> a -> (Pointer a, AList Integer a)
-    def mem addr val = 
-        if containsA mem addr
-            then error "addr already defined"
-            else (ptr, modMem)
-        where modMem = insertA mem (addr, val)
-              ptr = (P addr) 
+    def :: Memory -> Integer -> a -> (Pointer a, Memory)
 
+instance Mutable Value where
+        get mem (P addr) = lookupA mem addr
+        set mem (P addr) val = updateA mem (addr, val)
+        def mem addr val = 
+            if containsA mem addr
+                then error "addr already defined"
+                else (ptr, modMem)
+            where modMem = insertA mem (addr, val)
+                  ptr = (P addr) 
 
 -- Part 2: Chaining
 data StateOp a = StateOp (Memory -> (a, Memory))
--- MutationUser.hs
+++ MutationUser.hs
@@ -4,8 +4,10 @@ This file contains code which uses the mutation library found in Mutation.hs
 -}
 
 import Mutation (
-    get, set, def, Mutable, Pointer, Memory
+    get, set, def, Mutable, Memory, Pointer(..), Value(..)
     )
+
+--data Pointer a = P Integer
 
 -- | Takes a number <n> and memory, and stores two new values in memory:
 --   - the integer (n + 3) at location 100
@@ -13,5 +15,10 @@ import Mutation (
 --   Return the pointer to each stored value, and the new memory.
 --   You may assume these locations are not already used by the memory.
 pointerTest :: Integer -> Memory -> ((Pointer Integer, Pointer Bool), Memory)
-pointerTest = undefined
+pointerTest int mem = let
+    mem_100 = IntVal (int + 3)
+    mem_500 = BoolVal (int > 0)
+    (np_100, nmem_100) = def mem 100 mem_100 
+    (np_500, nmem_500) = def nmem_100 500 mem_500 in
+    ((P 100, P 500), nmem_500)
 
